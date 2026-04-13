7c5d751e0069dc4a4c310383060866733f1f807c
KirillMouraviev <kirill.mouraviev@yandex.ru>
2026-04-13 09:02:34 +0300

    Fixes to run VAMOS on RTX 5060 on Scout's bags

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ cd vamos_ws/src
 pip install -e trajectory_prediction/
 pip install -e trajectory_projection/
 
-cd vamos_src/src/vamos
+cd vamos/src/vamos/
 
 python dowload_hf_model.py --model_id "mateoguaman/vamos_navigation_only"
